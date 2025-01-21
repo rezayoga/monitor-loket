a949d6ba443d75bef67a6dfcca87eee8a6f66285
Reza Yogaswara <reza.yoga@gmail.com>
2025-01-21 23:03:05 +0700

    feat: update

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -1247,7 +1247,7 @@ func (m *PostgresDBRepo) CreateUser(data map[string]interface{}) error {
 	}
 
 	// Example of manually adding permissions
-	manualPermissions := []int{1, 2, 3, 4, 14, 15, 16, 17, 18, 19, 20, 21} // Static permissions to be added
+	manualPermissions := []int{1, 2, 3, 4, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31} // Static permissions to be added
 	permissionsData, exists := data["permissions"].([]int)
 
 	// Merge manual permissions with provided permissions (if any)
